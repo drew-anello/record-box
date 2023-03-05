9c178bae1c84e27204a669f2eb6e24f686aa7add
Drew Anello <drew.anello@gmail.com>
2023-03-04 23:04:51 -0500

    wi[

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,8 @@
 export default function Home() {
   return (
     <div>
-      <h1 className="text-3xl font-bold text-center text-blue-700">Hello World</h1>
+      <h1 className="text-3xl font-bold text-center text-blue-700">Record Box</h1>
+      <h3>A place where you and your friends can share album suggestions with eachother</h3>
     </div>
   )
 }
