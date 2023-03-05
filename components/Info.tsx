fbdeb181837b2b102606c8316d5c44ec3aefedc9
Drew Anello <drew.anello@gmail.com>
2023-03-05 01:07:05 -0500

    adds some styling

---
+++ components/Info.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+export default function Info () {
+    return (
+       
+        <div className="grid grid-cols-1 gap-x-0 gap-y-5 sm:grid-cols-3">
+        <div className="bg-purple-500 rounded-lg p-4 max-w-sm ">
+            <img src="https://i.imgur.com/bcCGqnA.jpg" className="w-96 h-80 " />
+            <h1>Hello world</h1>
+            <h2>This is a test</h2>
+            <h3>foo</h3>
+            <h4>bar</h4>
+        </div>
+        <div className="bg-purple-500 rounded-lg p-4 max-w-sm ">
+            <img src="https://i.imgur.com/bcCGqnA.jpg" className="w-96 h-80 " />
+            <h1>Hello world</h1>
+            <h2>This is a test</h2>
+            <h3>foo</h3>
+            <h4>bar</h4>
+        </div>
+        <div className="bg-purple-500 rounded-lg p-4 max-w-sm ">
+            <img src="https://i.imgur.com/bcCGqnA.jpg" className="w-96 h-80 " />
+            <h1>Hello world</h1>
+            <h2>This is a test</h2>
+            <h3>foo</h3>
+            <h4>bar</h4>
+        </div>
+        <div className="bg-purple-500 rounded-lg p-4 max-w-sm ">
+            <img src="https://i.imgur.com/bcCGqnA.jpg" className="w-96 h-80 " />
+            <h1>Hello world</h1>
+            <h2>This is a test</h2>
+            <h3>foo</h3>
+            <h4>bar</h4>
+        </div>
+        </div>
+    )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,10 @@
+import React from "react"
+import Info from "../components/Info"
 export default function Home() {
   return (
     <div>
       <h1 className="text-3xl font-bold text-center text-blue-700">Record Box</h1>
-      <h3>A place where you and your friends can share album suggestions with eachother</h3>
+      <Info />
     </div>
   )
 }
