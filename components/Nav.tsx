7b5edbbd105b7d389e7a634780c97ae073e4ffee
Drew Anello <drew.anello@gmail.com>
2023-03-06 00:21:31 -0500

    adds styling to nav updates form

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -3,14 +3,14 @@ import Link from "next/link";
 export default function Nav () {
     return(
         <div>
-            <ul>
-                <li>
+            <ul className="flex justify-between inline-block border border-blue-500 bg-blue-500 rounded py-2 px-4 bg-blue-500 hover:bg-blue-700 text-white">
+                <li className="mr-3 ">
                     <Link href="/">Home</Link>
                 </li>
-                <li>
+                <li className="mr-3">
                     <Link href="/add">Add A Record</Link>
                 </li>
-                <li>
+                <li className="mr-3">
                     <Link href="/about">About Record Box</Link>
                 </li>
             </ul>
--- pages/add.tsx
+++ pages/add.tsx
@@ -1,8 +1,60 @@
-export default function Add () {
-    return (
-        <div>
-            <h1>form</h1>
-            <h2>form</h2>
-        </div>
-    )
+import { useState } from 'react';
+
+export default function Add() {
+  const [albumCover, setAlbumCover] = useState('');
+  const [albumName, setAlbumName] = useState('');
+  const [albumYear, setAlbumYear] = useState('');
+  const [favoriteSong, setFavoriteSong] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(albumCover, albumName, albumYear, favoriteSong);
+    // You can submit the form data to your backend API here
+  };
+
+  const handleAlbumCoverChange = (event) => {
+    setAlbumCover(event.target.value);
+  };
+
+  const handleAlbumNameChange = (event) => {
+    setAlbumName(event.target.value);
+  };
+
+  const handleAlbumYearChange = (event) => {
+    setAlbumYear(event.target.value);
+  };
+
+  const handleFavoriteSongChange = (event) => {
+    setFavoriteSong(event.target.value);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label>
+          Album Cover:
+          <input type="text" value={albumCover} onChange={handleAlbumCoverChange} />
+        </label>
+      </div>
+      <div>
+        <label>
+          Album Name:
+          <input type="text" value={albumName} onChange={handleAlbumNameChange} />
+        </label>
+      </div>
+      <div>
+        <label>
+          Year Album Was Made:
+          <input type="text" value={albumYear} onChange={handleAlbumYearChange} />
+        </label>
+      </div>
+      <div>
+        <label>
+          Favorite Song on the Album:
+          <input type="text" value={favoriteSong} onChange={handleFavoriteSongChange} />
+        </label>
+      </div>
+      <button type="submit">Submit</button>
+    </form>
+  );
 }
