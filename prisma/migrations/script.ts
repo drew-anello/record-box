0ccd9a40983d4a9f57251e3f191e3121cde445a5
Drew Anello <drew.anello@gmail.com>
2023-03-10 10:41:53 -0500

    fixes script.ts

--- prisma/migrations/script.ts
+++ prisma/migrations/script.ts
@@ -1,35 +1,13 @@
-// const { PrismaClient } = require('@prisma/client')
-// const prisma = new PrismaClient()
-
-// app.post('/submit-form', async (req, res) => {
-//   const { albumCoverImage, albumName, releaseYear, favoriteSong, artistName, userName } = req.body
-
-//   try {
-//     const newPost = await prisma.post.create({
-//       data: {
-//         albumCoverImage,
-//         albumName,
-//         releaseYear,
-//         favoriteSong,
-//         artistName,
-//         userName,
-//       },
-//     })
-//     res.status(201).json({ message: 'Post created', post: newPost })
-//   } catch (error) {
-//     console.error(error)
-//     res.status(500).json({ message: 'Error creating post' })
-//   }
-// })
-import express from "express";
-import { PrismaClient } from "@prisma/client";
+import { NextApiRequest, NextApiResponse } from 'next';
+import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
-const app = express();
 
-app.use(express.json());
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method !== 'POST') {
+    return res.status(405).end();
+  }
 
-app.post("/submit-form", async (req, res) => {
   const { albumCoverImage, albumName, releaseYear, favoriteSong, artistName, userName } = req.body;
 
   try {
@@ -48,8 +26,4 @@ app.post("/submit-form", async (req, res) => {
     console.error(error);
     res.status(500).json({ message: "Error creating post" });
   }
-});
-
-const server = app.listen(3000, () => {
-  console.log("Server listening on port 3000");
-});
+}
